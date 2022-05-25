aef9deac87a0a55fad8767bbff2383e390866def
Casey Vu <vucas@oregonstate.edu>
2022-05-25 00:20:51 -0700

    Added 406 error check for GET /users.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,16 @@
 const user_ds = require("../datastore/users");
 
 const get_all_users = (req, res) => {
+  const accepts = req.accepts(["application/json"]);
+  // If none of these accepts are provided
+  if (!accepts) {
+    // it is False, send back a 406, Not Acceptable
+    res
+      .status(406)
+      .send({ Error: "Client must accept application/json" })
+      .end();
+    return;
+  }
   const users = user_ds.get_users().then((users) => {
     res.status(200).json({ results: users }).end();
   });
