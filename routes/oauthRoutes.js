3b0be3d2c51855b619fccde29e46362f24ca0982
Casey Vu <vucas@oregonstate.edu>
2022-05-25 18:40:38 -0700

    Renamed routes to oauthRoutes.

---
+++ routes/oauthRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const oauthController = require("../controllers/oauthController");
+const router = express.Router();
+
+router.get("/", oauthController.index);
+
+router.get("/authorize", oauthController.authorize);
+
+router.get("/oauth", oauthController.oauth);
+
+module.exports = router;
