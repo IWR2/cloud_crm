737ef9f87a04cece70a5add61a787b65147b7516
Casey Vu <vucas@oregonstate.edu>
2022-05-26 16:23:06 -0700

    Created route for get_a_service.

--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -6,4 +6,6 @@ router.post("/", serviceController.create_service);
 
 router.get("/", serviceController.get_all_services);
 
+router.get("/:id", serviceController.get_a_service);
+
 module.exports = router;
