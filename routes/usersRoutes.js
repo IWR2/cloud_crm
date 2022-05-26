5307adc7ad0760666b32d3dd676e3cac4938a5f6
Casey Vu <vucas@oregonstate.edu>
2022-05-25 19:28:08 -0700

    Added 405 errors to unsupported user methods.

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,4 +4,44 @@ const router = express.Router();
 
 router.get("/", userController.get_all_users);
 
+router.post("/", (req, res) => {
+  res.set("Accept", "POST");
+  res
+    .status(405)
+    .json({
+      Error: "Method not allowed",
+    })
+    .end();
+});
+
+router.delete("/", (req, res) => {
+  res.set("Accept", "DELETE");
+  res
+    .status(405)
+    .json({
+      Error: "Method not allowed",
+    })
+    .end();
+});
+
+router.put("/", (req, res) => {
+  res.set("Accept", "PUT");
+  res
+    .status(405)
+    .json({
+      Error: "Method not allowed",
+    })
+    .end();
+});
+
+router.patch("/", (req, res) => {
+  res.set("Accept", "PATCH");
+  res
+    .status(405)
+    .json({
+      Error: "Method not allowed",
+    })
+    .end();
+});
+
 module.exports = router;
