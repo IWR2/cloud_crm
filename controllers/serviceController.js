eb3191fe94efcf5ea35c27ab31202bf9d3aa7f9f
Casey Vu <vucas@oregonstate.edu>
2022-05-26 17:20:42 -0700

    Updated docstring for create_service. Created replace_a_service to validate attributes of a service and call put_service to update as service.

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,6 +1,19 @@
 const service_ds = require("../datastore/services");
 
-/* Gets all users from the datastore */
+/**
+ * Creates a new service and adds it to the Datastore, and returns a
+ * status 201 and a JSON representation of the newly created service
+ * with its self link.
+ * @param {Object} req Attributes of a service.
+ * @param {Object} res JSON represemtation of the newly created
+ * service with its self link.
+ * If the client sends an unsupported MIME type that is not JSON,
+ * it returns status 415.
+ * If the request and response are not application/json, it
+ * returns status 406.
+ * If any of the 3 required attributes are missing, not the
+ * correct datatype, or not valid, it returns status 400.
+ */
 const create_service = (req, res) => {
   // Check that the content-type from the client is in app/json
   if (req.get("content-type") !== "application/json") {
@@ -145,8 +158,102 @@ const get_a_service = (req, res) => {
   });
 };
 
+/**
+ * Creates a new service and adds it to the Datastore, and returns a
+ * status 201 and a JSON representation of the newly created service
+ * with its self link.
+ * @param {Object} req Attributes of a service.
+ * @param {Object} res JSON represemtation of the newly created
+ * service with its self link.
+ * If the client sends an unsupported MIME type that is not JSON,
+ * it returns status 415.
+ * If the request and response are not application/json, it
+ * returns status 406.
+ * If any of the 3 required attributes are missing, not the
+ * correct datatype, or not valid, it returns status 400.
+ */
+const replace_a_service = (req, res) => {
+  // Check that the content-type from the client is in app/json
+  if (req.get("content-type") !== "application/json") {
+    // It's not, reject it with status 415
+    res
+      .status(415)
+      .send({ Error: "Server only accepts application/json data" })
+      .end();
+    return;
+  }
+
+  const accepts = req.accepts(["application/json"]);
+  // If none of these accepts are provided
+  if (!accepts) {
+    // it is False, send back a 406, Not Acceptable
+    res
+      .status(406)
+      .send({ Error: "Client must accept application/json" })
+      .end();
+    return;
+  }
+
+  if (
+    req.body.name === undefined ||
+    req.body.type === undefined ||
+    req.body.price === undefined
+  ) {
+    res
+      .status(400)
+      .json({
+        Error:
+          "The request object is missing at least one of the required attributes",
+      })
+      .end();
+  } else {
+    const attributes = Object.keys(req.body);
+
+    if (attributes.length > 3) {
+      res
+        .status(400)
+        .json({
+          Error:
+            "The request object includes at least one unsupported attribute",
+        })
+        .end();
+      return;
+    }
+
+    if (req.body.price < 0 || typeof req.body.price != "number") {
+      res
+        .status(400)
+        .json({
+          Error: "The price attribute must be a non-negative number",
+        })
+        .end();
+      return;
+    }
+
+    service_ds
+      .put_service(req.params.id, req.body.name, req.body.type, req.body.price)
+      .then((new_service) => {
+        new_service[0].self =
+          req.protocol +
+          `://${req.get("host")}` +
+          `${req.baseUrl}/` +
+          `${new_service[0].id}`;
+        // Set the location header and return status 303.
+        res.location(new_service[0].self);
+        res.status(303).send({
+          id: new_service[0].id,
+          name: new_service[0].name,
+          type: new_service[0].type,
+          price: new_service[0].price,
+          self: new_service[0].self,
+        });
+      });
+  }
+};
+
 module.exports = {
   create_service,
   get_all_services,
   get_a_service,
+  replace_a_service,
 };
