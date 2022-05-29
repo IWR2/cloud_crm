baba2af918e72013eb269a01a26dacb7ee7eb46d
Casey Vu <vucas@oregonstate.edu>
2022-05-28 22:43:05 -0700

    Created replace_a_client to validate and replace a client's attributes.

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -267,8 +267,150 @@ const get_a_client_from_user = (req, res) => {
   }
 };
 
+/**
+ * Replaces all attributes of a client. The client retains any existing
+ * associations to a service and a user.
+ * @param {String} req To retrieve the authorization header and ID of a
+ * client and attributes of a client.
+ * @returns JSON of all attributes of a client.
+ * If the request and response are not application/json, it
+ * returns status 406.
+ * If the authorization is undefined or invalid, it sends status 401.
+ * If the authorization of the client is correct, it returns status 200
+ * and a list of all of the User's clients.
+ */
+const replace_a_client = (req, res) => {
+  let authorization = req.headers["authorization"];
+  if (authorization !== undefined) {
+    // Get the token value
+    let items = authorization.split(/[ ]+/);
+    if (items.length > 1 && items[0].trim() == "Bearer") {
+      let token = items[1];
+      // verify token
+      oauth2Client
+        .verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        })
+        .then((ticket) => {
+          const payload = ticket.getPayload();
+          const userid = payload["sub"];
+          const accepts = req.accepts(["application/json"]);
+          // If none of these accepts are provided
+          if (!accepts) {
+            // it is False, send back a 406, Not Acceptable
+            res
+              .status(406)
+              .send({ Error: "Client must accept application/json" })
+              .end();
+            return;
+          }
+
+          if (req.params.id === undefined || req.params.id === null) {
+            res
+              .status(400)
+              .json({
+                Error: "client_id must not be null",
+              })
+              .end();
+            return;
+          }
+
+          client_ds.get_client(req.params.id).then((client) => {
+            if (client[0] === undefined || client[0] === null) {
+              res
+                .status(404)
+                .json({ Error: "No client with this client_id exists" })
+                .end();
+              return;
+            } else if (userid != client[0].owner) {
+              res
+                .status(403)
+                .json({
+                  Error:
+                    "The user does not have access privileges to this client",
+                })
+                .end();
+            } else {
+              if (
+                req.body.name === undefined ||
+                req.body.contact_manager === undefined ||
+                req.body.email === undefined
+              ) {
+                res
+                  .status(400)
+                  .json({
+                    Error:
+                      "The request object is missing at least one of the required attributes",
+                  })
+                  .end();
+              } else {
+                const attributes = Object.keys(req.body);
+
+                if (attributes.length > 3) {
+                  res
+                    .status(400)
+                    .json({
+                      Error:
+                        "The request object includes at least one unsupported attribute",
+                    })
+                    .end();
+                  return;
+                }
+
+                const current_services = [];
+                // Assign a self link for each service
+                for (let i = 0; i < client[0].services.length; i++) {
+                  current_services.push({
+                    id: client[0].services.id,
+                    self:
+                      req.protocol +
+                      `://${req.get("host")}` +
+                      "/services/" +
+                      `${client[0].services[i].id}`,
+                  });
+                }
+
+                client_ds
+                  .put_client(
+                    client[0].id,
+                    req.body.name,
+                    req.body.contact_manager,
+                    req.body.email,
+                    client[0].services,
+                    userid
+                  )
+                  .then((new_client) => {
+                    res.status(201).send({
+                      id: new_client[0].id,
+                      name: new_client[0].name,
+                      contact_manager: new_client[0].contact_manager,
+                      email: new_client[0].email,
+                      services: current_services,
+                      owner: new_client[0].owner,
+                      self:
+                        req.protocol +
+                        `://${req.get("host")}` +
+                        `${req.baseUrl}/` +
+                        `${new_client[0].id}`,
+                    });
+                  });
+              }
+            }
+          });
+        })
+        .catch((err) => {
+          res.status(401).json({ Error: "Missing or invalid JWTs" }).end();
+        });
+    }
+  } else {
+    res.status(401).json({ Error: "Missing or invalid JWTs" }).end();
+  }
+};
+
 module.exports = {
   create_client,
   get_clients_from_user,
   get_a_client_from_user,
+  replace_a_client,
 };
