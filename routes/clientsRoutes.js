ef33cfa20595d964771400c65a6fe07e829a0524
Casey Vu <vucas@oregonstate.edu>
2022-05-28 17:46:48 -0700

    Created GET /clients/:id route to get a client with its ID.

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -3,6 +3,9 @@ const clientController = require("../controllers/clientController");
 const router = express.Router();
 
 router.post("/", clientController.create_client);
+
 router.get("/", clientController.get_clients_from_user);
+
+router.get("/:id", clientController.get_a_client_from_user);
 
 module.exports = router;
