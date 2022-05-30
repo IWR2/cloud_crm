e79f38b69d6e26cd965c0246820ea9b19bebeab6
Casey Vu <vucas@oregonstate.edu>
2022-05-29 21:37:10 -0700

    Created DELETE request to /clients/:id. Prevented DELETE request to all Clients.

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -12,6 +12,8 @@ router.put("/:id", clientController.replace_a_client);
 
 router.patch("/:id", clientController.update_a_client);
 
+router.delete("/:id", clientController.delete_a_client);
+
 router.put("/", (req, res) => {
   res.set("Accept", "PUT");
   res
@@ -24,6 +26,16 @@ router.put("/", (req, res) => {
 
 router.patch("/", (req, res) => {
   res.set("Accept", "PATCH");
+  res
+    .status(405)
+    .json({
+      Error: "Method not allowed",
+    })
+    .end();
+});
+
+router.delete("/", (req, res) => {
+  res.set("Accept", "DELETE");
   res
     .status(405)
     .json({
