de70fcd32a8b61662269fe1f2914173c20aa3b5f
Casey Vu <vucas@oregonstate.edu>
2022-05-29 20:58:48 -0700

    Created route PATCH to /client/:id. Prevent PATCH to root /client.

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -10,8 +10,20 @@ router.get("/:id", clientController.get_a_client_from_user);
 
 router.put("/:id", clientController.replace_a_client);
 
+router.patch("/:id", clientController.update_a_client);
+
 router.put("/", (req, res) => {
   res.set("Accept", "PUT");
+  res
+    .status(405)
+    .json({
+      Error: "Method not allowed",
+    })
+    .end();
+});
+
+router.patch("/", (req, res) => {
+  res.set("Accept", "PATCH");
   res
     .status(405)
     .json({
